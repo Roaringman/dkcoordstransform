3309d03e4d78e16f6aa2165ea2f54145a970cb57
Roar Engell <rgengell@gmail.com>
2020-04-28 22:03:53 +0200

    Added about information

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -19,51 +19,47 @@ function AboutPage() {
         <AboutNav>
           <StyledLink to="/">Transform</StyledLink>
         </AboutNav>
-        <AboutTitle>Important Things</AboutTitle>
+        <AboutTitle>About</AboutTitle>
         <AboutSection>
-          <AboutSubtitle>Something</AboutSubtitle>
+          <AboutSubtitle>What is this?</AboutSubtitle>
           <Paragraph>
-            I'm baby retro cray tofu, blog vegan gochujang four dollar toast pok
-            pok franzen selvage. Palo santo banh mi four dollar toast vaporware,
-            chambray lo-fi health goth next level. Roof party pug +1 beard
-            austin. Fashion axe chicharrones viral crucifix farm-to-table. Deep
-            v semiotics snackwave tousled chia, man braid next level. 3 wolf
-            moon vape 8-bit selvage, roof party kale chips you probably haven't
-            heard of them green juice health goth. Flannel iceland chillwave
-            taiyaki.
+            This project is based on an OpenSource{" "}
+            <a
+              href="https://docs.kortforsyningen.dk/#webproj"
+              target="_blank"
+              rel="noopener"
+            >
+              API
+            </a>{" "}
+            developed at the Danish Agency for Data Supply (SDFE). I am not
+            involved in the development of this API.
           </Paragraph>
 
           <Paragraph>
-            Asymmetrical vape forage photo booth. Letterpress flexitarian
-            coloring book migas pinterest crucifix. Sartorial lumbersexual
-            williamsburg 90's four loko PBR&B. Yuccie farm-to-table ennui,
-            subway tile truffaut flannel synth austin. Fashion axe etsy paleo
-            migas subway tile locavore. Kale chips selfies sartorial, swag
-            coloring book vinyl brooklyn yr kombucha deep v bicycle rights
-            mumblecore hoodie. Ramps vinyl snackwave portland DIY literally
-            brunch mixtape selfies.
+            Coordinate transformation for high precision positioning data is not
+            a simple task so I've tried to make the interface and workflow
+            simple, so you can verify your results and move on quickly.
           </Paragraph>
         </AboutSection>
         <AboutSection>
-          <AboutSubtitle>Someting else </AboutSubtitle>
+          <AboutSubtitle>I'm Here to Learn </AboutSubtitle>
           <Paragraph>
-            +1 bicycle rights leggings coloring book messenger bag kinfolk.
-            Subway tile scenester marfa, tbh portland enamel pin taxidermy
-            readymade copper mug jean shorts tousled ethical chambray lomo kogi.
-            Swag banjo brunch, direct trade meditation helvetica shoreditch
-            snackwave. Etsy pickled irony echo park before they sold out prism
-            venmo. Adaptogen snackwave sustainable +1 chambray pug. Microdosing
-            thundercats af offal distillery enamel pin craft beer bicycle rights
-            literally gluten-free fam authentic mixtape.
+            My goal with this front-end application is simply to play around
+            with technologies that are new or interesting to me. The three main
+            things I've used are:
           </Paragraph>
+          <ul>
+            <li>ReactJS</li>
+            <li>XStateJS</li>
+            <li>LeafletJS</li>
+          </ul>
 
           <Paragraph>
-            Air plant vaporware letterpress tofu, sustainable try-hard street
-            art fashion axe humblebrag vape pug cronut. Hell of slow-carb
-            chartreuse migas tattooed etsy, mumblecore hammock everyday carry
-            vinyl authentic. Photo booth fam paleo bespoke polaroid synth vape
-            food truck mlkshk tbh copper mug.
+            If you have comments, questions or tips for improvements, you are
+            welcome to contact me here: roargengell@gmail.com{" "}
           </Paragraph>
+
+          <Paragraph>I hope you enjoy using the front-end!</Paragraph>
         </AboutSection>
       </Article>
     </AboutMain>
